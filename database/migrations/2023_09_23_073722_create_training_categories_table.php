7de7edb5c78a9a2d4504d429ae91c8fd4b24b0d9
AndreasBinawan <bobiandreas2012@gmail.com>
2023-09-23 15:03:20 +0700

    finished migrations

---
+++ database/migrations/2023_09_23_073722_create_training_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainingCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('training_categories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('training_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+
+            $table->foreign('training_id')->references('id')->on('trainings')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('training_categories');
+    }
+}
